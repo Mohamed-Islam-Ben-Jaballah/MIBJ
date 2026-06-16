140891daa3ce7c1855a2d2801adf04c2fa2b70e2
Mohamed Islam Ben Jaballah <medislambenjaballah1@gmail.com>
2026-06-16 18:48:30 +0400

    remove emoji array hardcode 💎

--- js/effects.js
+++ js/effects.js
@@ -66,7 +66,6 @@
   var tokenCounter = document.getElementById('tokenCounter');
   var tokenCountEl = document.getElementById('tokenCount');
   var MAX_TOKENS = 7;
-  var EMOJI_CHARS = ['💎', '💎', '💎', '💎', '💎', '💎', '💎'];
 
   var tokensFound = 0;
   var hasSeenBubble = false;
@@ -105,7 +104,7 @@
       var pos = chosen[i];
       el.style.top = pos[0] + '%';
       el.style.left = pos[1] + '%';
-      el.textContent = EMOJI_CHARS[i];
+      el.textContent = '💎';
       document.body.appendChild(el);
       emojis.push(el);
       setupEmoji(el);
